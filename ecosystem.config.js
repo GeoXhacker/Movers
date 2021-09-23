a68586e45ea7368ffca013c01018d15ff993264a
GeoXhacker <mukisageophrey@gmail.com>
2021-09-23 20:59:26 +0300

    pm2 intergration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      script: "node bin/www",
-      watch: ".",
-    },
-  ],
+  script: "serve",
+  env: {
+    NODE_ENV: "production",
+  },
 
   deploy: {
     production: {
       user: "mukisa",
       host: ["167.172.76.19"],
       ref: "origin/main",
-      repo: "git@github.com:GeoXhacker/admin-backend.git",
-      path: "/home/mukisa/movers-admin",
-      "post-deploy": "npm install && pm2 start",
+      repo: "git@github.com:GeoXhacker/Movers.git",
+      path: "/home/mukisa/movers-client",
+      "post-deploy": "npm install && npm run build && pm2 serve www/index.html",
       // 'pre-deploy-local': '',
       // 'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       // 'pre-setup': ''
-      
     },
   },
 };
